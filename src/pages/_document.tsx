bf6ccc163a23b7d04abcbed4a9914153b317f8a6
brenolapreza <47190046+brenolapreza@users.noreply.github.com>
2022-03-17 15:47:34 -0300

    Alter document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -48,6 +48,15 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <div>
+        <button id="openChat" onclick="openTheChat()">Chat</button>
+        <div class="conteudo">
+            <div id="closeChat"><span onclick="openTheChat()" id="closeChat"></span></div>
+            <embed class="embedAkivaChat" type="text/html"
+                src="http://omni-chat.57c3f340730845a28a2d.westus2.aksapp.io/?company_id=12&chat_id=220"/>
+        </div>
+    </div>
+    <script src="http://20.206.127.201/files/web/js/v1/script.min.js"></script>
         </body>
       </Html>
     );
